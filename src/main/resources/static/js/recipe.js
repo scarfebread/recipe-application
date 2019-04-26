4d5774cdee1e9d53e8efd6e27d8922b76e0ee934
James Scarfe <james.scarfe@live.co.uk>
2019-04-26 10:16:11 +0100

    Finished HTML and CSS of home page

--- src/main/resources/static/js/recipe.js
+++ src/main/resources/static/js/recipe.js
@@ -76,34 +76,97 @@ document.addEventListener("DOMContentLoaded", function(event)
             }
         );
     };
-
-    function getRecipes()
-    {
-        let recipes;
-
-        fetch ("http://localhost:8080/api/recipe", {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            credentials: "same-origin",
-        }).then(
-            function (response) {
-                if (response.status !== 200)
-                {
-                    response.text().then(function(data) {
-                        // show error banner
-                    });
-
-                    return false;
-                }
-
-
-            }
-        ).catch(
-            function (error) {
-                // show error banner
-            }
-        );
-    }
 });
+
+function getRecipes()
+{
+    let recipes;
+
+    fetch ("http://localhost:8080/api/recipe", {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: "same-origin",
+    }).then(
+        function (response) {
+            if (response.status !== 200)
+            {
+                response.text().then(function(data) {
+                    // show error banner
+                });
+
+                return false;
+            }
+
+            // show recipes
+        }
+    ).catch(
+        function (error) {
+            // show error banner
+        }
+    );
+}
+
+function displayRecipe(recipe)
+{
+    let title = document.createElement('h3');
+    title.innerText = recipe.title;
+    title.className = 'recipeTitle';
+
+    let link = document.createElement('a');
+    link.href = `/recipe?id=${recipe.id}`;
+    link.appendChild(title);
+
+    let image = document.createElement('img');
+    image.className = 'recipeThumbnail';
+    image.src = recipe.image;
+
+    let cookTime = document.createElement('label');
+    cookTime.innerText = `Total time: ${recipe.totalCookTime}`;
+
+    let listItem = document.createElement('div');
+    listItem.className = 'recipeListItem';
+    listItem.appendChild(link);
+    listItem.appendChild(image);
+    listItem.appendChild(cookTime);
+    listItem.appendChild(document.createElement('br'));
+
+    for (let i = 0; i < recipe.rating; i++)
+    {
+        let star = document.createElement('span');
+        star.className = 'fa fa-star star checked';
+        listItem.appendChild(star);
+    }
+
+    for (let i = 0; i < (5 - recipe.rating); i++)
+    {
+        let star = document.createElement('span');
+        star.className = 'fa fa-star star';
+        listItem.appendChild(star);
+    }
+
+    let list = getElementById('allRecipesContainer');
+    list.appendChild(listItem);
+}
+
+function updateNavBar(recipes)
+{
+    let recipeDropdown = getElementById('recipeDropdown');
+
+    if (recipes.length === 0)
+    {
+        let noRecipesLabel = createElement('label');
+        noRecipesLabel.innerText = 'No recipes to display';
+        recipeDropdown.appendChild(noRecipesLabel);
+        return;
+    }
+
+    for (let recipe in recipes)
+    {
+        let recipeLink = createElement('a');
+        recipeLink.href = `/recipe?id=${recipe.id}`;
+        recipeLink.innerText = recipe.title;
+        recipeDropdown.appendChild(recipeLink);
+    }
+}
