95a0b4bb1f94665a266b4068ffb743f934c49daa
James Scarfe <james.scarfe@live.co.uk>
2020-11-28 14:31:08 +0000

    Rewrote the InventoryController and ShoppingListController

---
+++ src/main/kotlin/recipeapplication/controller/InventoryController.kt
@@ -0,0 +1,42 @@
+package recipeapplication.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.Errors
+import org.springframework.web.bind.annotation.*
+import recipeapplication.dto.InventoryItemDto
+import recipeapplication.exception.InventoryItemNotFoundException
+import recipeapplication.model.InventoryItem
+import recipeapplication.service.InventoryService
+import javax.validation.Valid
+
+@RestController
+@RequestMapping(path = ["/api/inventory"])
+class InventoryController @Autowired constructor(private val inventoryService: InventoryService) {
+    @GetMapping
+    fun getInventoryItems(): List<InventoryItem> {
+        return inventoryService.inventory
+    }
+
+    @PostMapping
+    fun createInventoryItem(@RequestBody inventoryItemDto: @Valid InventoryItemDto, errors: Errors): ResponseEntity<*> {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(400).body("Invalid inventory item")
+        }
+
+        return ResponseEntity
+                .status(201)
+                .body(inventoryService.createInventoryItem(inventoryItemDto))
+    }
+
+    @DeleteMapping
+    fun deleteInventoryItem(@RequestBody inventoryItemDto: InventoryItemDto): ResponseEntity<*> {
+        try {
+            inventoryService.deleteInventoryItem(inventoryItemDto)
+        } catch (e: InventoryItemNotFoundException) {
+            return ResponseEntity.status(404).body("Inventory item not found")
+        }
+
+        return ResponseEntity.status(202).body("Deleted successfully")
+    }
+}
---
+++ src/main/kotlin/recipeapplication/controller/ShoppingListController.kt
@@ -0,0 +1,78 @@
+package recipeapplication.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.Errors
+import org.springframework.web.bind.annotation.*
+import recipeapplication.dto.ShoppingListItemDto
+import recipeapplication.exception.IngredientDoesNotExistException
+import recipeapplication.exception.ShoppingListItemNotFoundException
+import recipeapplication.model.ShoppingListItem
+import recipeapplication.service.InventoryService
+import recipeapplication.service.ShoppingListService
+import javax.validation.Valid
+
+@RestController
+@RequestMapping(path = ["/api/shopping-list"])
+class ShoppingListController @Autowired constructor(private val shoppingListService: ShoppingListService, private val inventoryService: InventoryService) {
+    @GetMapping
+    fun getShoppingListItems(): List<ShoppingListItem> {
+        return shoppingListService.shoppingList
+    }
+    
+    @PostMapping
+    fun createShoppingListItem(@RequestBody shoppingListItemDto: @Valid ShoppingListItemDto, errors: Errors): ResponseEntity<*> {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(400).body("Invalid shopping list item")
+        }
+
+        return ResponseEntity
+                .status(201)
+                .body(shoppingListService.createShoppingListItem(shoppingListItemDto))
+    }
+
+    @DeleteMapping
+    fun deleteShoppingListItem(@RequestBody shoppingListItemDto: ShoppingListItemDto): ResponseEntity<*> {
+        try {
+            shoppingListService.deleteShoppingListItem(shoppingListItemDto)
+        } catch (e: ShoppingListItemNotFoundException) {
+            return ResponseEntity.status(404).body("Shopping list item not found")
+        }
+
+        return ResponseEntity.status(202).body("Deleted successfully")
+    }
+
+    @PostMapping("/purchase")
+    fun purchaseIngredient(@RequestBody shoppingListItemDto: ShoppingListItemDto): ResponseEntity<*> {
+        try {
+            val shoppingListItem = shoppingListService.getShoppingListItem(shoppingListItemDto)
+
+            inventoryService.createInventoryItem(shoppingListItem.ingredient)
+            shoppingListService.deleteShoppingListItem(shoppingListItemDto)
+        } catch (e: ShoppingListItemNotFoundException) {
+            return ResponseEntity.status(404).body("Shopping list item not found")
+        }
+
+        return ResponseEntity.status(201).body("Created")
+    }
+
+    @DeleteMapping("/remove/{ingredientId}")
+    fun removeFromShoppingList(@PathVariable(value = "ingredientId") ingredientId: Long): ResponseEntity<*> {
+        return try {
+            shoppingListService.removeFromShoppingList(ingredientId)
+            ResponseEntity.status(202).body("Removed successfully")
+        } catch (e: ShoppingListItemNotFoundException) {
+            ResponseEntity.status(404).body("Shopping list entry not found")
+        }
+    }
+
+    @PostMapping("/add/{ingredientId}")
+    fun addToShoppingList(@PathVariable(value = "ingredientId") ingredientId: Long): ResponseEntity<*> {
+        return try {
+            shoppingListService.addToShoppingList(ingredientId)
+            ResponseEntity.status(201).body("Added successfully")
+        } catch (e: IngredientDoesNotExistException) {
+            ResponseEntity.status(404).body("Ingredient not found")
+        }
+    }
+}
