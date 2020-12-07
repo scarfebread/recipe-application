b546df134f399ba44f4baf7cad05b7b130850e89
James Scarfe <james.scarfe@live.co.uk>
2020-12-07 19:53:39 +0000

    Rewrote ShoppingListItem

---
+++ src/main/kotlin/recipeapplication/model/ShoppingListItem.kt
@@ -0,0 +1,18 @@
+package recipeapplication.model
+
+import java.io.Serializable
+import javax.persistence.*
+
+@Entity
+@Table(name = "shopping_list")
+class ShoppingListItem : Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @OneToOne
+    lateinit var user: User
+
+    @OneToOne(cascade = [CascadeType.PERSIST])
+    lateinit var ingredient: Ingredient
+}
